fbff9889ca2ab1f9d28b4eb9d0e210a043803848
TestSheepNZ <mike_talks@hotmail.com>
2016-08-16 21:49:59 +1200

    All working as per spec

--- Selenium 3 - testing with Test/src/aardvarkTest.java
+++ Selenium 3 - testing with Test/src/aardvarkTest.java
@@ -4,6 +4,8 @@ import java.util.concurrent.TimeUnit;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -13,16 +15,96 @@ import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import com.google.common.base.Verify;
+
 public class aardvarkTest {
+	
+	@Test
+	public void thisTestWillFail() 
+	{
+		
+		System.out.println("thisTestWillFail");
+		
+		//This test is set to fail
+		
+        // Create a new instance of the Chrome driver
+        // This copies all the information about the page we've loaded into
+        // the page_item object
+        //WebDriver page_item = new FirefoxDriver();
+    	WebDriver driver = new ChromeDriver();
+    	
+        // Open our target page ... a previous blog article
+        driver.get("http://testsheepnz.blogspot.co.nz/2016/07/im-hoping-that-this-blog-will-have-most.html");
+
+        /*
+         * THIS NEXT STEP WILL FAIL
+         */
+                
+        //Assert we're on the right page - this will fail outright if not
+        assertTrue ("Verify we're on the right blog page - THIS WILL FAIL",
+        		driver.getPageSource().contains("THIS TEXT IS WRONG"));
+        
+        /*
+         * THE TEST WILL NOT GET FURTHER THAN THIS
+         */
+        
+        
+		// Verify if there is a blue aardvark in the page
+		try { 
+			assertTrue("Verify the page contains that blue aardvark", 
+					(driver.getPageSource().contains("blue aardvark"))  );
+				}
+			catch (Error e) {
+				System.out.println("ERROR");
+				System.out.println(e.toString());
+			}
+       
+		// Verify if there is a blue aardvark in the page
+		try { 
+			assertTrue("Verify the page contains that red aardvark", 
+					(driver.getPageSource().contains("red aardvark"))  );
+				}
+			catch (Error e) {
+				System.out.println("ERROR");
+				System.out.println(e.toString());
+			}
+             
+        // We are now going to capture an element of the current page
+        // I've used developer tools to find the ID for it
+        // This is the main body of the piece (and excludes comments & title)
+        WebElement element = driver.findElement(By.id("post-body-8711143007795160191"));
+       
+		// Verify if there is a blue aardvark in the post body
+		try { 
+			assertTrue("Verify the page contains that blue aardvark", 
+					(element.getText().contains("blue aardvark"))  );
+				}
+			catch (Error e) {
+				System.out.println("ERROR");
+				System.out.println(e.toString());
+			}
+        
+		// Verify if there is a blue aardvark in the post body
+		try { 
+			assertTrue("Verify the page contains that red aardvark", 
+					(element.getText().contains("red aardvark"))  );
+				}
+			catch (Error e) {
+				System.out.println("ERROR");
+				System.out.println(e.toString());
+			}
+
+	}
 
 	@Test
 	public void createCommentAndReview() 
 	{
+		
+		System.out.println("createCommentAndReview");
 
-		// Create a new instance of the Firefox driver
+		// Create a new instance of the Chrome driver
 		// This copies all the information about the page we've loaded into
 		// the driver object
-		//WebDriver driver = new FirefoxDriver();
 		WebDriver driver = new ChromeDriver();
 
 		// Open our target page ... a previous blog article
@@ -33,8 +115,25 @@ public class aardvarkTest {
 
 		driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
 
-		// Switch to the frame that comments are in
-		driver.switchTo().frame(4);
+        //Find the frame containing "enter your comment"
+        java.util.List<WebElement> iframes
+        	= driver.findElements(By.tagName("iframe"));
+		int chosenFrame = -1;
+		for (int i=0 ; i < iframes.size() ; i++)
+		{
+			//This moves you back out of the frame you've just been in
+			driver.switchTo().defaultContent();
+			System.out.println("Check frame");
+			
+			//Select the ith frame
+			driver.switchTo().frame(i);
+			if (driver.getPageSource().contains("Enter your comment"))
+			{
+				chosenFrame = i;
+				System.out.println("Found frame");
+				break;
+			}          
+        }
 
 		// Select to comment as "anonymous"
 		Select select = new Select(driver.findElement(By.name("identityMenu")));
@@ -49,15 +148,90 @@ public class aardvarkTest {
 		// Select to preview
 		element = driver.findElement(By.id("postCommentPreview"));
 		element.submit();
+		
+		//Assert that the stringComment can be seen on the page
+		assertTrue("Assert that we can see " + stringComment + "on page", 
+				driver.getPageSource().contains(stringComment));
 
-		// Confirm get told "Anonymous said" / Comment
-		if (driver.getPageSource().contains("Anonymous") && driver.getPageSource().contains("said")) {
-			System.out.println("Confirmed: Anonymous said");
-		}
-
-		if (driver.getPageSource().contains(stringComment)) {
-			System.out.println("Confirmed: " + stringComment);
-		}
+		// Verify that we are told "Anonymous said" 
+		try { 
+			assertTrue("Verify the page contains that 'Anonymous said'", 
+					(driver.getPageSource().contains("Anonymous") && driver.getPageSource().contains("said"))  );
+				}
+			catch (Error e) {
+				System.out.println("ERROR");
+				System.out.println(e.toString());
+			}
 
 	}
+	
+	@Test
+	public void checkForRedAndBlue() 
+	{
+		
+		System.out.println("checkForRedAndBlue");
+		
+        // Create a new instance of the Chrome driver
+        // This copies all the information about the page we've loaded into
+        // the page_item object
+        //WebDriver page_item = new FirefoxDriver();
+    	WebDriver driver = new ChromeDriver();
+    	
+        // Open our target page ... a previous blog article
+        driver.get("http://testsheepnz.blogspot.co.nz/2016/07/im-hoping-that-this-blog-will-have-most.html");
+
+        //Assert we're on the right page - this will fail outright if not
+        assertTrue ("Verify we're on the right blog page",
+        		driver.getPageSource().contains("I'm hoping that this blog will have the most comments"));
+        
+		// Verify if there is a blue aardvark in the page
+		try { 
+			assertTrue("Verify the page contains that blue aardvark", 
+					(driver.getPageSource().contains("blue aardvark"))  );
+				}
+			catch (Error e) {
+				System.out.println("ERROR");
+				System.out.println(e.toString());
+			}
+       
+		// Verify if there is a blue aardvark in the page
+		try { 
+			assertTrue("Verify the page contains that red aardvark", 
+					(driver.getPageSource().contains("red aardvark"))  );
+				}
+			catch (Error e) {
+				System.out.println("ERROR");
+				System.out.println(e.toString());
+			}
+             
+        // We are now going to capture an element of the current page
+        // I've used developer tools to find the ID for it
+        // This is the main body of the piece (and excludes comments & title)
+        WebElement element = driver.findElement(By.id("post-body-8711143007795160191"));
+       
+		// Verify if there is a blue aardvark in the post body
+		try { 
+			assertTrue("Verify the page contains that blue aardvark", 
+					(element.getText().contains("blue aardvark"))  );
+				}
+			catch (Error e) {
+				System.out.println("ERROR");
+				System.out.println(e.toString());
+			}
+        
+		// Verify if there is a blue aardvark in the post body
+		try { 
+			assertTrue("Verify the page contains that red aardvark", 
+					(element.getText().contains("red aardvark"))  );
+				}
+			catch (Error e) {
+				System.out.println("ERROR");
+				System.out.println(e.toString());
+			}
+
+	}
+	
+	
+
+	
 }
